4ea71015792f13cb917b6fadd612ad652dfd8259
Aayush Kumar Gupta <aayushgupta120305@gmail.com>
2025-11-29 00:25:06 +0530

    Update layout title and add Google Analytics scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
-
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,21 +15,36 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Vfound.in | Developer Tools & Smart Resume Checker",
+  title: "Vfound.in | Developer Builder",
   description:
     "Hyper-advanced resume analysis and job compatibility checker with built-in latest IT sector news and developer tools.",
 };
 
-
 export default function RootLayout({ children }) {
-   return (
+  return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          {/* Google Analytics */}
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-RFS8XFXTZ7"
+          />
+          <Script id="ga-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RFS8XFXTZ7');
+            `}
+          </Script>
+        </head>
+
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <Navbar />
           {children}
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
