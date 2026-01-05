d203b051e420bdf6a5d1cca55a3f8530c7c2bf03
AayushKGupta12 <aayushgupta120305@gmail.com>
2026-01-05 17:33:36 +0530

    done

--- lib/supabase-client.tsx
+++ lib/supabase-client.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-    "https://ttmokypbvoquoonaiizb.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0bW9reXBidm9xdW9vbmFpaXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5NTI0ODgsImV4cCI6MjA4MDUyODQ4OH0.BqhvkamylNnOCqPZsvYz2iT-AJtMxzTKHXdj7oNJrlM"
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 );
