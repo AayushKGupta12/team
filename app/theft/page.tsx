e596bf72d3535281e301a3bc12da2cb64c0a34e5
AayushKGupta12 <aayushgupta120305@gmail.com>
2026-01-20 22:54:27 +0530

    Blocked list added

---
+++ app/theft/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Blocked_users from '../components/Blocked_users'
+
+const page = () => {
+  return (
+    <div>
+        <Blocked_users/>
+      
+    </div>
+  )
+}
+
+export default page
