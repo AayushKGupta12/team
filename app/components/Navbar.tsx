0def3a67aa07602c5ed5e6893257f6203b4d0f9a
AayushKGupta12 <aayushgupta120305@gmail.com>
2026-02-07 20:26:05 +0530

    Signin and Signup experiment

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -142,7 +142,7 @@ export default function Navbar(): React.JSX.Element {
                   <SignInButton>
                     <div className="flex gap-2">
                     <a
-                        href="#roles"
+                        href="/sign-in"
                         className="border relative h-8 py-0.5 p-2 text-black text-base font-bold overflow-hidden bg-white rounded-4xl transition-all duration-200 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-700 before:to-blue-400 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0">
                         Sign in
                     </a>
@@ -151,7 +151,7 @@ export default function Navbar(): React.JSX.Element {
                   <SignUpButton>
                     <div className="flex gap-2">
                     <a
-                        href="#roles"
+                        href="/sign-up"
                         className="border relative h-8 py-0.5 p-2 text-black text-base font-bold overflow-hidden bg-white rounded-4xl transition-all duration-200 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-blue-700 before:to-blue-400 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0">
                         Sign up
                     </a>
