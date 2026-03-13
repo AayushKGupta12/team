c38f130d15b513970ecb2359eb6ddfaeb37547da
AayushKGupta12 <aayushgupta120305@gmail.com>
2026-03-13 19:59:55 +0530

    Updated

--- app/components/ExtenionDashboard.tsx
+++ app/components/ExtenionDashboard.tsx
@@ -93,7 +93,7 @@ export default function ExtensionDashboard() {
     setLoading(true);
     setError(null);
     try {
-      const res = await fetch( `${process.env.NEXT_PUBLIC_API_URL_EXT}/api/extension-dashboard`, {
+      const res = await fetch( `${process.env.NEXT_PUBLIC_API_URL}/api/extension-dashboard`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
